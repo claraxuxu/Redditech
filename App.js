996a6f1b9fd2d4b36a215916bb88f789a5ff1579
ClaraXU <claraxu1201@gmail.com>
2021-10-19 12:07:03 +0200

    new react native repo

--- App.js
+++ App.js
@@ -1,46 +1,112 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Navigator from './navigator/homestack';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
 
-export default () => {
+import React from 'react';
+import type {Node} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const Section = ({children, title}): Node => {
+  const isDarkMode = useColorScheme() === 'dark';
   return (
-    <Navigator />
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
   );
 };
 
+const App: () => Node = () => {
+  const isDarkMode = useColorScheme() === 'dark';
 
-// const discovery = {
-//   authorizationEndpoint: 'https://www.reddit.com/api/v1/authorize.compact',
-//   tokenEndpoint: 'https://www.reddit.com/api/v1/access_token',
-// };
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
 
-// export default function App() {
-//   const [request, response, promptAsync] = useAuthRequest(
-//     {
-//       clientId: 'CLIENT_ID',
-//       scopes: ['identity'],
-//       redirectUri: makeRedirectUri({
-//         // For usage in bare and standalone
-//         native: 'your.app://redirect',
-//       }),
-//     },
-//     discovery
-//   );
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Header />
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <Section title="Step One">
+            Edit <Text style={styles.highlight}>App.js</Text> to change this
+            screen and then come back to see your edits.
+          </Section>
+          <Section title="See Your Changes">
+            <ReloadInstructions />
+          </Section>
+          <Section title="Debug">
+            <DebugInstructions />
+          </Section>
+          <Section title="Learn More">
+            Read the docs to discover what to do next:
+          </Section>
+          <LearnMoreLinks />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
-//   React.useEffect(() => {
-//     if (response?.type === 'success') {
-//       const { code } = response.params;
-//       }
-//   }, [response]);
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
 
-//   return (
-//     <Button
-//       disabled={!request}
-//       title="Login"
-//       onPress={() => {
-//         promptAsync();
-//         }}
-//     />
-//   );
-// }
+export default App;
