1608269ead6ec0841ad8c20245ea4ce994c60cd3
Neyross <thibault.bordenave@epitech.eu>
2021-10-28 13:17:22 +0200

    dropdown filters for posts

--- component/Home.js
+++ component/Home.js
@@ -7,27 +7,15 @@ import {
     Text
 } from 'react-native';
 import { Card } from 'react-native-elements';
-
+import SelectDropdown from 'react-native-select-dropdown';
 
 const Home = ({ navigation }) => {
     const [isLoaded, setLoaded] = useState(false);
-    const filter = (filter) => {
-      let filterName;
-      switch (filter) {
-        case 'top':
-          filterName = 'top';
-          break;
-        case 'best':
-          filterName = 'best';
-        case 'new':
-          filterName = 'new';
-        default:
-          break;
-      }
-    }
-    const getPostNoLog = async () => {
+    const filters =['top', 'hot', 'new', 'best']
+    const [filter, setFilter] = useState(filters[0])
+    const getPostNoLog = async () => { 
       try {
-          const res = await fetch("https://www.reddit.com/r/all/" + 'top' + ".json?limit=50", {
+          const res = await fetch("https://www.reddit.com/r/all/" + filter + ".json?limit=50", {
               method: 'GET',
           });
           postJson = await res.json();
@@ -40,31 +28,41 @@ const Home = ({ navigation }) => {
   }
   useEffect(() => {
     getPostNoLog()
-  }, [])
+  }, [filter])
   if (isLoaded === true)
     return (
-      <ScrollView>
-        {global.elem.map((item, index) => (
-          <Card key={index}>
-             <Card.Title>{item.data.subreddit_name_prefixed}</Card.Title>      
-              <View>
-                <Text style={styles.author}> Publié par {item.data.author} </Text>
-                <Text style={styles.inputText}> {item.data.title} </Text>
-                <Image style={{width: item.data.thumbnail_width, height: item.data.thumbnail_height}} source={{uri: item.data.thumbnail}}/>
-                <View style={styles.voteContainer}>
-                  <Text style={styles.voteText}>
-                    <Image source={require('../assets/up.png')} style={styles.voteImg}/>
-                    {item.data.score}
-                    <Image source={require('../assets/down.png')} style={styles.voteImg}/>
-                    {item.data.num_comments}
-                    <Image source={require('../assets/comment.png')} style={styles.voteImg}/>
-                  </Text>
+      <View>
+        <SelectDropdown
+        data = {filters}
+        onSelect={(selectedItem, index) => {
+          setFilter(filters[index])
+          setLoaded(false)
+        }}
+        defaultValue= {filter}
+        />
+        <ScrollView>
+          {global.elem.map((item, index) => (
+            <Card key={index}>
+              <Card.Title>{item.data.subreddit_name_prefixed}</Card.Title>      
+                <View>
+                  <Text style={styles.author}> Publié par {item.data.author} </Text>
+                  <Text style={styles.inputText}> {item.data.title} </Text>
+                  <Image style={{width: item.data.thumbnail_width, height: item.data.thumbnail_height}} source={{uri: item.data.thumbnail}}/>
+                  <View style={styles.voteContainer}>
+                    <Text style={styles.voteText}>
+                      <Image source={require('../assets/up.png')} style={styles.voteImg}/>
+                      {item.data.score}
+                      <Image source={require('../assets/down.png')} style={styles.voteImg}/>
+                      {item.data.num_comments}
+                      <Image source={require('../assets/comment.png')} style={styles.voteImg}/>
+                    </Text>
+                  </View>
                 </View>
-              </View>
-            </Card>
-          )
-        )}
-      </ScrollView>
+              </Card>
+            )
+          )}
+        </ScrollView>
+      </View>
     );
   else
     return (
