918137f02b25dcbbd88b36f53482a3ce8ed0267c
clara <yuhui.xu@epitech.eu>
2021-10-29 16:33:58 +0200

    profile edit page, no function yet

--- component/Profile.js
+++ component/Profile.js
@@ -1,22 +1,66 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Text, Image, TouchableOpacity, Modal } from 'react-native';
+import { StyleSheet, View, Text, Image, TouchableOpacity, Modal, TextInput, Switch } from 'react-native';
+import axios from 'react-native-axios';
 
 const Profile = ({ navigation }) => {
     const img = global.resBody.icon_img
     const [img1, rien] = img.split('?')
     let img2 = {uri: img1};
     const [modalOpen, setModalOpen] = useState(false);
+    const [isEnabled, setIsEnabled] = useState(false);
+    const toggleSwitch = () => setIsEnabled(previousState => !previousState);
 
     return (
         <View style={styles.container}>
-            <Modal visible={false}>
+            <Modal visible={modalOpen} animationType='slide'>
                 <View style={styles.modalContent}>
-                    <Text>MODAL MODAL</Text>
+                    <View style={styles.button}>
+                        <TouchableOpacity
+                        style={styles.modalBut}
+                        onPress={() => setModalOpen(false)}>
+                            <Text style={styles.saveText}>Back</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity
+                        style={styles.modalBut}
+                        onPress={() => setModalOpen(false)}>
+                            <Text style={styles.saveText}>Save</Text>
+                        </TouchableOpacity>
+                    </View>
+
+                    <View style={styles.changeBar}>
+                        <Text  style={styles.info}>Name :</Text>
+                        <TextInput 
+                            placeholder={global.resBody.name}
+                            placeholderTextColor="#E5E5E5"
+                            style={styles.changeInput}
+                        />
+                    </View>
+                    <View style={styles.changeBar}>
+                        <Text style={styles.info}>Description : </Text>
+                        <TextInput 
+                            placeholder={global.resBody.subreddit.public_description}
+                            placeholderTextColor="#E5E5E5"
+                            style={styles.changeInput}
+                        />
+                    </View>
+                    <View style={styles.changeBar}>
+                        <Text style={styles.info}>Over18? : </Text>
+                        <Switch
+                            trackColor={{ false: "#767577", true: "#81b0ff" }}
+                            thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
+                            ios_backgroundColor="#3e3e3e"
+                            onValueChange={toggleSwitch}
+                            value={isEnabled}
+                        >
+                        </Switch>
+                    </View>
                 </View>
             </Modal>
             <View style={styles.pad} 
                 blurRadius={1}>
-                <TouchableOpacity style={styles.editButton}>
+                <TouchableOpacity
+                    onPress={() => setModalOpen(true)}
+                    style={styles.editButton}>
                 {/* faire un popup de edit */}
                     <Image style={styles.edit}
                     source={require('../assets/edit.png')} />
@@ -25,7 +69,9 @@ const Profile = ({ navigation }) => {
                 <Text style={styles.info}>Description : {global.resBody.subreddit.public_description}</Text>
                 <Text style={styles.info}>Friends : {global.resBody.num_friends}</Text>
                 <Text style={styles.info}>Karma : {global.resBody.link_karma}</Text>
-                <Text style={styles.info}>Coins : {global.resBody.coins}</Text>
+                {global.resBody.over_18 === false ? 
+                    <Text style={styles.info}>Over_18 : No</Text>
+                : <Text style={styles.info}>Over_18: Yes</Text>}
             </View>
             <Image style={styles.pp} source={img2} />
         </View>
@@ -37,6 +83,30 @@ const styles = StyleSheet.create({
         flex: 1,
         margin: 5,
         alignItems: 'center'
+    },
+    button: {
+        justifyContent: 'space-between',
+        flexDirection: 'row',
+        margin: 10
+    },
+    changeBar: {
+        margin: 10,
+        flexDirection: 'row',
+        width: 300,
+    },
+    changeItem: {
+        marginTop: 13
+    },
+    changeInput: {
+        margin: 5,
+        height: 40,
+        borderBottomWidth: 1,
+        borderBottomColor: "#404040"
+    },
+    saveText: {
+        color: "#000",
+        fontWeight: 'bold',
+        fontSize: 15
     },
     pad: {
         marginTop: 70,
@@ -57,8 +127,8 @@ const styles = StyleSheet.create({
     },
     pp: {
         position: 'absolute',
-        width: 100,
-        height: 100,
+        width: 90,
+        height: 90,
         borderRadius: 50,
         borderColor: "#000"
     },
--- component/subreddit.js
+++ component/subreddit.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
     ScrollView,
     View,
@@ -11,38 +11,46 @@ import {
 import axios from 'react-native-axios';
 import { Card } from 'react-native-elements';
 import Home from './Home';
+import SelectDropdown from 'react-native-select-dropdown';
 
 function SubReddit({ navigation }) {
   const [isTopped, setTopped] = useState(false);
   const [subreddit, setSubreddit] = useState('');
+  const filters =['top', 'hot', 'new', 'best']
+  const [filter, setFilter] = useState(filters[0])
 
-  function getTop(subreddit) {
-      if (subreddit) {
-        axios.get('https://www.reddit.com/r/'+ subreddit +'/top.json?limit=10')
-        .then(function (response) {
-          global.top = response;
-          global.d = top.data.data.children
-          console.log(subreddit)
-          setTopped(true)
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
-      }
+  const getTop = async () => {
+    axios.get('https://www.reddit.com/r/'+ subreddit +'/' + filter + '.json?limit=10')
+    .then(function (response) {
+      global.top = response;
+      global.d = top.data.data.children
+      setTopped(true)
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 
-      axios.get('https://www.reddit.com/r/' + subreddit + '/about.json')
-      .then(function (response) {
-        global.subCount = response.data.data.header_img;
-        console.log(subCount);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
+    axios.get('https://www.reddit.com/r/' + subreddit + '/about.json')
+    .then(function (response) {
+      global.subCount = response.data.data;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
   }
+  useEffect(() => {
+    getTop()
+  }, [filter])
 
     return (
         <View style={styles.container}>
           <View style={styles.inputView}>
+            <TouchableOpacity
+              onPress={() => setTopped(false)}
+              style={styles.backButton}
+              >
+              <Image style={styles.back} source={require('../assets/back.png')} />
+            </TouchableOpacity>
             <TextInput 
               style={styles.inputText}
               placeholder="Searching..." 
@@ -51,7 +59,6 @@ function SubReddit({ navigation }) {
               value={subreddit}
             >
             </TextInput>
-
             <View style={styles.clickView}>
               {subreddit ?
                 <TouchableOpacity
@@ -62,22 +69,39 @@ function SubReddit({ navigation }) {
               : null}
             </View>
           </View>
+          
 
-          {global.d ? 
+          {isTopped === true ? 
             <ScrollView style={styles.info_box}>
               <View style={styles.info_subView}>
               {global.subCount ? 
-                <Image style={styles.jpgs} source={{uri: global.subCount}} />
+                  <Image style={styles.jpgs} source={{uri: global.subCount.header_img}} />
               : null}
                 <View style={styles.info_text}>
                   <Text style={styles.info_sub}> {d[0].data.subreddit_name_prefixed}</Text>
+                  <Text style={styles.info_sub}> {global.subCount.public_description}</Text>
                   <Text style={styles.info_sub}> {d[0].data.subreddit_subscribers} Members</Text>
+                  <SelectDropdown
+                    data = {filters}
+                    onSelect={(selectedItem, index) => {
+                      setFilter(filters[index])
+                      setTopped(false)
+                    }}
+                    defaultValue= {filter}
+                    buttonStyle={styles.drop}
+                    buttonTextStyle={styles.textDrop}
+                  />
                 </View>
               </View>
               {global.d.map((item, index) => (
                 <Card key={index}>
                   <View style={styles.posts}>
-                    <Image style={styles.jpgs} key={index} source={{ uri: item.data.thumbnail }} />
+                    <Text style={styles.title}> {item.data.title} </Text>
+                    <Text style={styles.des}> Publie par {item.data.author} </Text>
+                    {item.data.thumbnail ?
+                      <Image style={styles.jpgs} key={index} source={{ uri: item.data.thumbnail }} />
+                    : null}
+                    <Text style={styles.info_sub}> {item.data.num_comments} comments</Text>
                   </View>
                 </Card>
                 )
@@ -93,13 +117,31 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
   },
+  backButton: {
+    marginLeft: '-20%',
+    marginRight: '5%',
+    marginTop: 5,
+  },
+  back: {
+    height: 30,
+    width: 30
+  },
+  drop: {
+    width: 70,
+    height: 30,
+    borderRadius: 10,
+    backgroundColor: "#B3C7D1"
+  },
+  textDrop: {
+    fontSize: 15
+  },
   search: {
     marginTop: '20%',
     width: 300,
   },
   inputView:{
-    marginTop: '3%',
-    width:"80%",
+    marginTop: '10%',
+    width:"75%",
     backgroundColor:"#465881",
     borderRadius:15,
     height:40,
@@ -109,6 +151,7 @@ const styles = StyleSheet.create({
   },
   inputText: {
     height: 40,
+    width:"85%",
     color: "#fff"
   },
   info_box: {
@@ -128,11 +171,20 @@ const styles = StyleSheet.create({
   info_text: {
     flexDirection: 'column'
   },
+  title: {
+    fontWeight: 'bold',
+    color: "#000",
+    textAlign: 'center',
+  },
+  des: {
+    textAlign: 'right',
+    color: "#303030",
+    fontSize: 12
+  },
   info_sub: {
     color: "#000"
   },
   clickView: {
-    marginLeft: '50%',
     marginTop: '4%'
   },
   icon: {
